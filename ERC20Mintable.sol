e61ee39b8902d6ef3810c7a674c850d374d69d95
chaojin752 <chaojin752@gmail.com>
2020-04-28 22:17:48 +0100

    Adding shouldPause to avoid pausing all the addresses first time the coin is minted

--- ERC20Mintable.sol
+++ ERC20Mintable.sol
@@ -17,15 +17,26 @@ contract ERC20Mintable is ERC20, MinterRole {
      *
      * - the caller must have the `MinterRole`.
      */
-    function mint(address account, uint256 amount) public onlyMinter returns (bool) {
+    function mint(address account, uint256 amount, bool shouldPause) public onlyMinter returns (bool) {
         _mint(account, amount);
 
         // First time being minted? Then let's ensure
         // the token will remain paused for now
-        if (!_isPaused(account)) {
+        if (shouldPause && !_isPaused(account)) {
             _pauseAccount(account);
         }
 
         return true;
+    }
+
+    /**
+     * @dev See `ERC20._mint`.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MinterRole`.
+     */
+    function mint(address account, uint256 amount) public onlyMinter returns (bool) {
+        return mint(account, amount, true);
     }
 }
